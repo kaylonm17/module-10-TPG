e2db862d79b26fb77592fb4d442eab6ea614d15c
Kaylon Myers <kaylonmyers2001@gmail.com>
2023-02-20 22:10:34 -0600

    fixed intern and manager.js

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ const writefile = require('./src/write-file')
 const { Manager, managerQuestionsArr } = require('./lib/Manager');
 const { Engineer, engineerQuestionsArr } = require('./lib/Engineer');
 const { Intern, internQuestionsArr } = require('./lib/Intern')
+
+const employeeArr = []
+
+const init = () => { managerQuestions () } 
+
+const managerQuestions = () => {
+    inquirer.createPromptModule(engineerQuestionsArr)
+    .then(( answers ) => (
+        answers = new Engineer(answers.name, answers.id, answers.github)
+    ))
+}
