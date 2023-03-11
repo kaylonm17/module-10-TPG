92fb4ca7f4813cc2f4d6807d396f1bedb05b522f
Kaylon Myers <kaylonmyers2001@gmail.com>
2023-03-11 16:45:43 -0600

    final push

--- index.js
+++ index.js
@@ -7,13 +7,41 @@ const { Manager, managerQuestionsArr } = require('./lib/Manager');
 const { Engineer, engineerQuestionsArr } = require('./lib/Engineer');
 const { Intern, internQuestionsArr } = require('./lib/Intern')
 
-const employeeArr = []
+const employeesArr = []
 
-const init = () => { managerQuestions () } 
+const init = () => { managerQuestions() } 
 
 const managerQuestions = () => {
+    inquirer.createPromptModule(managerQuestionsArr)
+    .then(( answers ) => {
+        answers = new Manager(answers.name, answers.email, answers.officeNumber)
+        employeesArr.push(answers);
+        return employeePrompt();
+    })
+}
+
+const engineerQuestions = () => {
     inquirer.createPromptModule(engineerQuestionsArr)
-    .then(( answers ) => (
+    .then(( answers ) => {
         answers = new Engineer(answers.name, answers.id, answers.github)
-    ))
+        employeesArr.push(answers);
+        return employeePrompt();
+    })
+}
+
+const internQuestions = () => {
+    inquirer.createPromptModule(internQuestionsArr)
+    .then(( answers ) => {
+        answers = new Engineer(answers.name, answers.id, answers.github)
+        employeesArr.push(answers);
+        return employeePrompt();
+    })
+}
+
+const employeePrompt = () => {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'employeeType',
+        message: "What type "
+    }])
 }
